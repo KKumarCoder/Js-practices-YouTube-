374507ec8b9b8dc958d4323c515d1fa89999de01
KKumarCoder <155147414+KKumarCoder@users.noreply.github.com>
2024-01-06 16:44:07 +0000

    conversation of DataTypeFitter

---
+++ .devcontainer/01_basics /03_conversationOperation.js
@@ -0,0 +1,38 @@
+//let score="33ABC";
+//let score=null;
+// let score=undefined;
+// let score=true;
+let score="krishna";
+
+// //const {score}=req.body
+// console.log(typeof score);
+// console.log(typeof (score));
+// let valnumber=Number(score);
+// console.log( typeof valnumber);
+// console.log( valnumber);//NaN dataType
+//"33"=>33
+//"33aAbd=>NaN
+//true=>1
+//false=>0
+//let isLogIn=1;
+let isLogIn="krishna"
+let booleanIsLogInt=Boolean(isLogIn);
+console.log(booleanIsLogInt);
+console.log(isLogIn);
+
+//1=>true;0=>false;
+//" "=>false;
+//"krishna"=>true
+
+let somenumber=33;
+let stringnumber=String(somenumber);
+console.log(stringnumber);
+console.log( typeof stringnumber);
+
+
+
+
+
+
+
+
